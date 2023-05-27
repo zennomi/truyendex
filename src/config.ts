a955ea9458a3408d51adf1818ae58d475effc188
zen-pc <zennomi.kun@gmail.com>
2023-05-27 12:04:27 +0700

    hotfix

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 const isProductionEnv = process.env.NODE_ENV === "production"
 
 const config = {
-    corsUrl: isProductionEnv ? "" : "https://cors.zenno.moe",
+    corsUrl: isProductionEnv ? "https://cors.zenno.moe" : "https://cors.zenno.moe",
     isProductionEnv,
 }
 
