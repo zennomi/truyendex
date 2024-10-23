889c31b6cdd4d58b5eb3984ac089571c27af85b9
kienht2 <kienht2@gmail.com>
2024-10-24 02:08:57 +0700

    chore: add eslint and prettier
    Since linting is not enabled from start, with strict profile, a fair
    amount of error is reported. Bailing out from building on linting error
    is disabled for now.

--- next.config.js
+++ next.config.js
@@ -12,6 +12,12 @@ const nextConfig = {
         fetches: {
             fullUrl: true,
         },
+    },
+    // Currently having a lot of errors, just ignore them for now //
+    // [kamii0909]: ping me if you upgrade eslint to 9 (btw only next 15 is
+    // compatible with eslint 9), I will write the flat config instead. 
+    eslint: {
+        ignoreDuringBuilds: true
     }
 }
 
