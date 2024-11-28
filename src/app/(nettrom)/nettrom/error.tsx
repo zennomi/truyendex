633129b09c48326e9d97805f5aa596d64dc2ac22
zen-lenovo <zennomi.kun@gmail.com>
2024-11-28 23:26:26 +0700

    feat: issue report for user

--- src/app/(nettrom)/nettrom/error.tsx
+++ src/app/(nettrom)/nettrom/error.tsx
@@ -1,5 +1,6 @@
 "use client"; // Error components must be Client Components
 
+import { Constants } from "@/constants";
 import { useEffect } from "react";
 
 export default function Error({
@@ -16,9 +17,12 @@ export default function Error({
 
   return (
     <div>
-      <h2>Đã có lỗi gì xảy ra</h2>
+      <h2 className="mb-1">
+        Đã có lỗi gì đó xảy ra 😭 Bạn thông cảm dự án phi lợi nhuận nên lỗi là
+        chuyện bình thường á 🥺
+      </h2>
       <button
-        className="btn btn-danger"
+        className="btn btn-danger mb-2"
         onClick={
           // Attempt to recover by trying to re-render the segment
           () => reset()
@@ -26,6 +30,17 @@ export default function Error({
       >
         Thử lại
       </button>
+      <div>Hoặc chụp màn hình lỗi phía dưới và gửi cho mình nhé 👇</div>
+      <a
+        className="btn btn-warning"
+        href={Constants.Routes.report}
+        target="_blank"
+      >
+        Gửi cho mình
+      </a>
+      <div className="mt-2 break-all rounded bg-black p-3">
+        {JSON.stringify(error, Object.getOwnPropertyNames(error))}
+      </div>
     </div>
   );
 }
