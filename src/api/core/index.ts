a84db46d3eaeaf675f220628ce43db0942c74fa4
zen-lenovo <zennomi.kun@gmail.com>
2024-12-26 23:18:51 +0700

    update: comment form UI

--- src/api/core/index.ts
+++ src/api/core/index.ts
@@ -1,3 +1,4 @@
 export * from "./axios";
 export * as Series from "./series";
 export * as User from "./user";
+export * as Comment from "./comment";
--- src/app/(nettrom)/nettrom/truyen-tranh/[mangaId]/page.tsx
+++ src/app/(nettrom)/nettrom/truyen-tranh/[mangaId]/page.tsx
@@ -1,3 +1,4 @@
+import CommentSection from "@/components/nettrom/binh-luan/comment-section";
 import TopTitles from "@/components/nettrom/trang-chu/top-titles";
 import Manga from "@/components/nettrom/truyen-tranh/manga";
 
@@ -10,6 +11,7 @@ export default function TruyenTranh({
     <div className="grid gap-[40px] lg:grid-cols-[2fr_1fr]">
       <div>
         <Manga mangaId={params.mangaId} />
+        <CommentSection />
       </div>
       <div>
         <TopTitles />
