79dc486a84ef8ca6f867fce831d3350a4620aeb2
zen-lenovo <zennomi.kun@gmail.com>
2025-01-12 00:18:46 +0700

    fix: crash on ios 15 16

--- src/components/nettrom/Markdown.tsx
+++ src/components/nettrom/Markdown.tsx
@@ -1,7 +1,7 @@
 import ReactMarkdown from "react-markdown";
 // markdown plugins
 import rehypeRaw from "rehype-raw";
-import remarkGfm from "remark-gfm";
+// import remarkGfm from "remark-gfm";
 import rehypeHighlight from "rehype-highlight";
 
 export default function Markdown({ content }: { content: string }) {
@@ -10,7 +10,7 @@ export default function Markdown({ content }: { content: string }) {
       rehypePlugins={[
         rehypeRaw,
         rehypeHighlight,
-        [remarkGfm, { singleTilde: false }],
+        // [remarkGfm, { singleTilde: false }],
       ]}
       className="w-full [&_a]:text-web-title [&_a]:hover:text-web-titleLighter [&_pre]:whitespace-pre-wrap [&_pre]:break-words"
     >
