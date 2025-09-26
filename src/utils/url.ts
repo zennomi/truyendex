8a821435ae7cadd45aecc707f86292ab5c682676
Zennomi <43679352+zennomi@users.noreply.github.com>
2025-09-26 22:11:35 +0700

    fix: update getCoverArt method to use proxy for image URLs, improving accessibility in regions where MangaDex is blocked

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,16 +1,10 @@
 import { MangadexApi } from "@/api";
 import { Constants } from "@/constants";
 
-const IMAGE_RESIZE_URL = "https://resizer.f-ck.me";
-
 export class UrlUtils {
   getSearchNetTromUrl(options: MangadexApi.Manga.GetSearchMangaRequestOptions) {
     const queryString = MangadexApi.Utils.buildQueryStringFromOptions(options);
     return `${Constants.Routes.nettrom.search}${queryString.replaceAll("[]", "")}#results`;
-  }
-
-  getResizeImgUrl(url: string, queryParams?: string) {
-    return `${IMAGE_RESIZE_URL}/?url=${url}&${queryParams ?? ""}`;
   }
 
   getBackendUrl() {
